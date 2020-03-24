ba282d09a3b85f5930fc8d7254da37ccb253dfcd
hfpp2012 <hfpp2012@gmail.com>
2020-03-24 22:16:54 +0800

    12_pipe

--- src/users/dto/register.input.ts
+++ src/users/dto/register.input.ts
@@ -1,19 +1,23 @@
 import { InputType, Field } from '@nestjs/graphql';
-import { IsEmail, MinLength } from 'class-validator';
+import { IsEmail, MinLength, IsNotEmpty } from 'class-validator';
 
 @InputType()
 export class RegisterInput {
   @Field()
   @IsEmail()
+  @IsNotEmpty()
   email: string;
 
   @Field()
+  @IsNotEmpty()
   @MinLength(6)
   username: string;
 
   @Field()
+  @IsNotEmpty()
   password: string;
 
   @Field()
+  @IsNotEmpty()
   confirmPassword: string;
 }
